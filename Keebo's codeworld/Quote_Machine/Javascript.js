50dd7203ca57ac11c68fd68a2984e3628dd16781
KIngTJai <132505377+KIngTJai@users.noreply.github.com>
2023-08-02 16:41:56 -0400

    Update Javascript.js

---
+++ Keebo's codeworld/Quote_Machine/Javascript.js
@@ -0,0 +1,13 @@
+var randomNumber = Math.floor(Math.random()* 20);
+quotes[14]
+var quotes = [
+	"I was told to put a quote here, but I don't know any good quotes",
+	"MY EYESSSSSSS",
+	"CLATU! VERATA! NMPHMHHIAHGHU",
+	"This here is my BOOMSTICK",
+	"POKEMON! Gotta eat em' all!"
+]
+function newQuote() {
+	var randomNumber = Math.floor(Math.random() * (quotes.length));
+	document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber]
+}
